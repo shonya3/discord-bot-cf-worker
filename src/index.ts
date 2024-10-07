dbd2570d8b848c241477adc0774dbbdedc0534af
shonya3 <poeshonya3@gmail.com>
2024-10-07 22:53:49 +0300

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,46 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+import { getCuteUrl } from './reddit';
+const DISCORD_PUBLIC_KEY = '6905bc84c7ad6093a46b69f7bb3a33bd30935a6fcb6f76283146b62d3d6562d3';
+import { InteractionResponseType, InteractionType, verifyKey } from 'discord-interactions';
+
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+	async fetch(request, env): Promise<Response> {
+		const { isValid, interaction } = await verify(request, env);
+		if (!isValid || !interaction) {
+			return new Response('Bad request signature.', { status: 401 });
+		}
+
+		if (interaction.type === InteractionType.PING) {
+			return new Response(
+				JSON.stringify({
+					type: InteractionResponseType.PONG,
+				}),
+				{ headers: 'application/json' }
+			);
+		}
+
+		return new Response(
+			JSON.stringify({
+				type: 4,
+				data: {
+					content: await getCuteUrl(),
+				},
+			}),
+			{ headers: { 'content-type': 'application/json' } }
+		);
 	},
 } satisfies ExportedHandler<Env>;
+
+const verify = async (request: Request, env: Env) => {
+	const signature = request.headers.get('x-signature-ed25519');
+	const timestamp = request.headers.get('x-signature-timestamp');
+	const body = await request.text();
+	const isValidRequest = signature && timestamp && (await verifyKey(body, signature, timestamp, DISCORD_PUBLIC_KEY));
+	if (!isValidRequest) {
+		return { isValid: false };
+	}
+
+	return { interaction: JSON.parse(body), isValid: true };
+};
