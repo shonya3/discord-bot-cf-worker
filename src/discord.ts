1249acd89f1592852c457f6a6aff922c39798152
shonya3 <poeshonya3@gmail.com>
2024-10-13 22:02:33 +0300

    add discord message types

--- src/discord.ts
+++ src/discord.ts
@@ -37,3 +37,125 @@ export class DiscordResponse extends JsonResponse {
 		super(obj);
 	}
 }
+
+export class DiscordMessageResponse extends DiscordResponse {
+	constructor(content: string) {
+		super({ type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE, data: { content } });
+	}
+}
+
+type FetchChannelMessagesArgs = {
+	channel_id: string;
+	bot_token: string;
+	limit: number;
+};
+
+export async function fetch_channel_messages({ channel_id, bot_token, limit }: FetchChannelMessagesArgs): Promise<Array<Message>> {
+	try {
+		const response = await fetch(`https://discord.com/api/v10/channels/${channel_id}/messages?limit=${limit}`, {
+			headers: {
+				Authorization: `Bot ${bot_token}`,
+				'Content-Type': 'application/json',
+			},
+		});
+
+		if (!response.ok) {
+			throw new Error(`Error fetching messages: ${response.statusText}`);
+		}
+
+		return await response.json();
+	} catch (err: unknown) {
+		if (err instanceof Error) {
+			throw err.message;
+		} else throw new Error(`Error fetching messages ${err}`);
+	}
+}
+
+type Message = {
+	type: number;
+	content: string;
+	mentions: Array<Mention>;
+	attachments: Array<unknown>;
+	embeds: Array<unknown>;
+	timestamp: string;
+	edited_timestamp: string | null;
+	flags: number;
+	components: Array<unknown>;
+	id: string;
+	channel_id: string;
+	author: Author;
+	pinned: boolean;
+	mention_everyone: boolean;
+	tts: boolean;
+	thread?: Thread;
+	reactions?: Array<Reaction>;
+};
+
+type Thread = {
+	/**
+	 * The id of the parent message.
+	 */
+	parent_id: string;
+	id: string;
+	type: number;
+	last_message_id: string;
+	flags: number;
+	guild_id: string;
+	name: string;
+	rate_limit_per_user: number;
+	bitrate: number;
+	user_limit: number;
+	rtc_region: unknown | null;
+	owner_id: Author['id'];
+	thread_metadata: {
+		archived: boolean;
+		archive_timestamp: string;
+		auto_archive_duration: number;
+		locked: boolean;
+		create_timestamp: string;
+	};
+};
+
+type Mention = {
+	id: string;
+	username: string;
+	avatar: string | null;
+	public_flags: number;
+	flags: number;
+	banner: string | null;
+	accent_color: string | null;
+	global_name: string;
+	avatar_decoration_data: string | null;
+	banner_color: string | null;
+	clan: string | null;
+};
+
+type Author = {
+	id: string;
+	username: string;
+	avatar: string;
+	descriminator: string;
+	public_flags: number;
+	flags: number;
+	banner: string | null;
+	accent_color: string | null;
+	global_name: string;
+	avatarr_decoration_data: null;
+	banner_color: string | null;
+	clan: string | null;
+};
+type Reaction = {
+	emoji: Emoji;
+	count: number;
+	count_details: { burst: number; normal: number };
+	burst_colors: Array<unknown>;
+	me_burst: boolean;
+	burst_me: boolean;
+	me: boolean;
+	burst_count: number;
+};
+
+type Emoji = {
+	id: string;
+	name: string;
+};
